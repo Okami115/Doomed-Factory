cda150d96ea3c35b8b590f257305e9b6a3ef6168
Dario <otrantodario@gmail.com>
2024-05-25 16:25:56 -0300

    Add Footsteps and Music

--- Assets/Scripts/Player/PlayerMovemnet.cs
+++ Assets/Scripts/Player/PlayerMovemnet.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerMovemnet : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Rigidbody rb;
-    private Vector3 movePlayer;
+    private float _timeBetweenSteps;
+    private Vector3 _movePlayer;
     private void Start()
     {
         PlayerInputsRead.Moving += PlayerMove;
@@ -14,17 +17,27 @@ public class PlayerMovemnet : MonoBehaviour
     {
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoVertical = Input.GetAxis("Vertical");
-
-
+        
+        
         Vector3 movimiento = new Vector3(movimientoHorizontal, 0.0f, movimientoVertical);
         movimiento = Camera.main.transform.TransformDirection(movimiento);
         movimiento.y = 0.0f; 
-
+        
         rb.AddForce(movimiento * speed);
+        
+        if (rb.velocity != Vector3.zero && _timeBetweenSteps < 0.0f)
+        {
+            AkSoundEngine.PostEvent("Play_Player_FootSteps",gameObject);
+            _timeBetweenSteps = 0.5f;
+        }
+        else
+        {
+            _timeBetweenSteps -= Time.deltaTime;
+        }
     }
 
     private void PlayerMove(Vector3 move)
     {
-        movePlayer = move;
+        _movePlayer = move;
     }
 }
