55ce7774630facb2c2a311b8b9e5c78d9ff10608
OkamiDesu <tomaspp515@gmail.com>
2024-06-05 18:20:35 -0300

    Set new light

--- Assets/Scripts/Keys/KeyDebug.cs
+++ Assets/Scripts/Keys/KeyDebug.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class KeyDebug : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(0);
+        UnityEngine.Cursor.lockState = CursorLockMode.Confined;
     }
 }
