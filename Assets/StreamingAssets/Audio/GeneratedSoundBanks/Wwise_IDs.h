9ec68b48479545b7649b61a533c44676c617ecc9
Ary <116613839+AryAudio@users.noreply.github.com>
2024-06-21 22:28:01 -0300

    Add Wwise Audio Assets & Events
    Agrego Pasos e interacciones con celular y puertas

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -15,9 +15,35 @@ namespace AK
     {
         static const AkUniqueID PLAY_DEFAULTENVIROMENT = 3051613610U;
         static const AkUniqueID PLAY_KEYPICKUP = 885077343U;
-        static const AkUniqueID PLAY_PHONEINTERACT = 1811953720U;
         static const AkUniqueID PLAY_PLAYER_FOOTSTEPS = 98439365U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_DOOR_METAL_LOCK = 3136413185U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_DOOR_METAL_UNLOCK = 1657049032U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_DOOR_WOOD_LOCK = 1936894595U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_DOOR_WOOD_UNLOCK = 199723134U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_PHONE_LIGHT_FAIL = 2920634929U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_PHONE_LIGHT_OFF = 1974193772U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_PHONE_LIGHT_ON = 2178443270U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_PHONE_SCREEN_FAIL = 3834016641U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_PHONE_SCREEN_LOCK = 577666544U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_PHONE_SCREEN_UNLOCK = 3588694021U;
+        static const AkUniqueID PLAY_SFX_PLAYER_INTERACT_PHONE_SWITCHSCREEN = 1036060318U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYER_FOOTSTEPS_MATERIAL
+        {
+            static const AkUniqueID GROUP = 3262912638U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace PLAYER_FOOTSTEPS_MATERIAL
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
