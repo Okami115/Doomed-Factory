f742631e8748982bc1f0f16ac30ebea1ee377fb1
OkamiDesu <Tomaspp515@gmail.com>
2024-10-08 22:00:02 -0300

    Update doors

--- Assets/Scripts/Keys/Door.cs
+++ Assets/Scripts/Keys/Door.cs
@@ -8,10 +8,23 @@ public class Door : MonoBehaviour, IInteractable
     [SerializeField] private Keys _doorKey;
     [SerializeField] private Animator doorTimeline;
     [SerializeField] private float waitTime;
+    [SerializeField] private Transform doorCamera;
     
     private bool isPlayerInRange;
     private bool isDoorOpen;
     private bool corrutineRuning = false;
+
+    private void Update()
+    {
+        Quaternion direction = Quaternion.Inverse(transform.rotation) * Camera.main.transform.rotation;
+
+        doorCamera.localEulerAngles = new Vector3(direction.eulerAngles.x, 
+                                                    direction.eulerAngles.y - 270, 
+                                                    direction.eulerAngles.z);
+
+        Vector3 distance = transform.InverseTransformPoint(Camera.main.transform.position);
+        //doorCamera.localPosition = -new Vector3(distance.x, -distance.y, distance.z);
+    }
 
     public void Interact(List<Keys> keysList)
     {
