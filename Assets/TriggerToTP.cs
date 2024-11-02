6a01ca5587d34c93339a9cc1e019fc1cd5f446f0
OkamiDesu <tomaspp515@gmail.com>
2024-11-01 23:08:53 -0300

    Implement Loop Art in the game Loop

--- Assets/TriggerToTP.cs
+++ Assets/TriggerToTP.cs
@@ -6,6 +6,7 @@ public class TriggerToTP : MonoBehaviour
     int counter = 0;
     [SerializeField] private PlayerMovementNavMesh player;
     [SerializeField] private Image background;
+    [SerializeField] private GameObject Loop;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,8 +14,10 @@ public class TriggerToTP : MonoBehaviour
 
         if((counter % 2) == 0)
         {
+            Loop.SetActive(true);
             player.isTPOn = true;
             background.color = new Color(0, 0, 0, 255);
+            Destroy(this.gameObject);
         }
     }
 }
