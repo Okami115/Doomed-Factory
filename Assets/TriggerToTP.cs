46468bac2907806f9efc214417641ddbad204a32
Dario <otrantodaro@gmail.com>
2024-11-04 02:16:37 -0300

    Fix Water Sound

--- Assets/TriggerToTP.cs
+++ Assets/TriggerToTP.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,14 +8,24 @@ public class TriggerToTP : MonoBehaviour
     [SerializeField] private PlayerMovementNavMesh player;
     [SerializeField] private Image background;
     [SerializeField] private GameObject Loop;
+    [SerializeField] private List<GameObject> LoopDisableObjects = new List<GameObject>();
 
     private void OnCollisionEnter(Collision collision)
     {
         counter++;
 
-        if((counter % 2) == 0)
+        if ((counter % 2) == 0)
         {
             Loop.SetActive(true);
+            if (LoopDisableObjects != null && LoopDisableObjects.Count > 0)
+            {
+                foreach (GameObject objec in LoopDisableObjects)
+                {
+                    if (objec.activeInHierarchy)
+                        objec.SetActive(false);
+                }
+            }
+
             player.isTPOn = true;
             background.color = new Color(0, 0, 0, 255);
             Destroy(this.gameObject);
