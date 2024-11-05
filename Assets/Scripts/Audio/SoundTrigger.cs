a9afe73a62c266ce93f0404c6efae15265ab782c
DaroOtr <otrantodaro@gmail.com>
2024-11-05 20:18:47 -0300

    Add Sound Trigger OneShot Check

--- Assets/Scripts/Audio/SoundTrigger.cs
+++ Assets/Scripts/Audio/SoundTrigger.cs
@@ -4,10 +4,14 @@ using UnityEngine.Events;
 
 public class SoundTrigger : MonoBehaviour
 {
+    [SerializeField] private bool _isOneShot;
     [SerializeField] private UnityEvent _unityEvent;
 
     private void OnTriggerEnter(Collider other)
     {
         _unityEvent?.Invoke();
+
+        if (_isOneShot)
+            Destroy(gameObject);
     }
 }
