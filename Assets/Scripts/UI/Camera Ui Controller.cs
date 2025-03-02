ebd38c58a1adb049a8176e960b4275e349c26a53
Dario <otrantodaro@gmail.com>
2025-03-02 17:11:04 -0300

    Fix Camera Rect Time

--- Assets/Scripts/UI/Camera Ui Controller.cs
+++ Assets/Scripts/UI/Camera Ui Controller.cs
@@ -3,22 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraUiController : MonoBehaviour
 {
     [SerializeField] private GameObject cameraRecCircle;
     [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private RawImage cameraRec;
+    [SerializeField] private List<GameObject> objectsLights;
     private float currentTime;
     private float flickTime = 1.5f;
     private bool _startflashingCorrutine = false;
     private bool lightEnabled = true;
     private bool alreadyPlayed = false;
+    private bool alreadyPlayedStart = false;
 
     private void OnEnable()
     {
-        alreadyPlayed = false;
-        lightEnabled = true;
-        cameraRecCircle.SetActive(true);
+        if (!alreadyPlayedStart)
+            StartCoroutine(StartCameraUi());
+    }
+
+    private void OnDisable()
+    {
+        lightEnabled = false;
+        cameraRec.enabled = false;
+        timeText.gameObject.SetActive(false);
+        cameraRecCircle.SetActive(false);
+        foreach (GameObject light in objectsLights)
+        {
+            light.SetActive(false);
+        }
     }
 
     private void Update()
@@ -51,5 +66,20 @@ public class CameraUiController : MonoBehaviour
         cameraRecCircle.SetActive(true);
         yield return null;
         _startflashingCorrutine = false;
+    }
+
+    public IEnumerator StartCameraUi()
+    {
+        yield return new WaitForSeconds(0.5f);
+        cameraRec.enabled = true;
+        cameraRecCircle.SetActive(true);
+        timeText.gameObject.SetActive(true);
+        lightEnabled = true;
+        foreach (GameObject light in objectsLights)
+        {
+            light.SetActive(true);
+        }
+        alreadyPlayedStart = false;
+        yield return null;
     }
 }
